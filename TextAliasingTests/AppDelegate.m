ea85ff80c56b52957b271db104e8444a0cff30ce
silvansky <v.silvansky@gmail.com>
2013-04-25 15:43:36 +0400

    Initial Commit

---
+++ TextAliasingTests/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  TextAliasingTests
+//
+//  Created by Valentine Gorshkov on 25.04.13.
+//  Copyright (c) 2013 silvansky. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ TextAliasingTests/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TextAliasingTests
+//
+//  Created by Valentine Gorshkov on 25.04.13.
+//  Copyright (c) 2013 silvansky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
